b6869e3b1ba17f68c37db6c63a684d90ad2ec9e8
Rahul Goyal <rahul.goyal223@live.com>
2018-08-13 22:20:43 +0530

    Initial Code - Day1

--- Rahul/Runnerup.py
+++ Rahul/Runnerup.py
@@ -1,7 +1,13 @@
 # runner-up score
-l = eval(input("Enter list:"))
+print("Enter list:", end=" ")
+l = [int(x) for x in input().split()]
 l.sort(reverse=True)
-for i in range(1, len(l)):
+if len(l)>1:
+ for i in range(1, len(l)):
     if (l[i - 1] > l[i]):
         print(l[i])
         break
+elif len(l)==1:
+    print(l[0])
+else:
+    print("Enter valid value")
