dc06c5d58dcc8c1b7efcc4dca402b5a4cf52b626
Rahul Goyal <rahul.goyal223@live.com>
2018-08-15 11:29:54 +0530

    Initial Code - Day2

--- Rahul/Rock-Paper-Scissors.py
+++ Rahul/Rock-Paper-Scissors.py
@@ -3,29 +3,29 @@ againPlay = True
 s = ['Rock', 'Scissors', 'Paper']
 while againPlay:
     userInput = input('''Enter a input from ['Rock', 'Scissors','Paper']:''')
+    if userInput not in s:
+        raise SystemExit("invalid input")
+    loser = 'You'
     comp = choice(s)
     while userInput==comp:
         comp = choice(s)
-
     if userInput == 'Rock':
         if comp == 'Scissors':
-            print("Computer's input is", comp, ": You win")
-        elif comp == 'Paper':
-            print("Computer's input is", comp, ": You lose")
+            loser = 'computer'
 
     elif userInput == 'Scissors':
         if comp == 'Paper':
-            print("Computer's input is", comp, ": You win")
-        elif comp == 'Rock':
-            print("Computer's input is", comp, ": You lose")
+            loser = 'computer'
 
     elif userInput == 'Paper':
         if comp == 'Rock':
-            print("Computer's input is", comp, ": You win")
-        elif comp == 'Scissors':
-            print("Computer's input is", comp, ": You lose")
+            loser = 'computer'
+
+    if loser=='computer':
+        print("Computer's input is", comp, ": You win")
     else:
-        print("Invalid input")
+        print("Computer's input is", comp, ": You Lose")
+
 
     playAgain = input("Enter yes to re-play:")
     if playAgain == 'yes':
--- Rahul/character frequency.py
+++ Rahul/character frequency.py
@@ -1,12 +1,8 @@
 s = input('Sample String :')
-stringSet = set()
-stringSet.update(s)
-frequency = {}
 
-for i in stringSet:
-    k = 0
-    for j in s:
-        if (i == j):
-            k += 1
-    frequency[i] = k
-print('Expected Result : ', frequency)
+d={}
+
+for x in s:
+    d[x]=d.get(x,0)+1
+
+print(d)
