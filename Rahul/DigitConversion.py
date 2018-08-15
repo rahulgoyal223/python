e746597fae55ee099db9b0e633d3ffedefaf55f9
Rahul Goyal <rahul.goyal223@live.com>
2018-08-15 16:50:41 +0530

    Initial Code - Day3

--- Rahul/DigitConversion.py
+++ Rahul/DigitConversion.py
@@ -1,7 +1,7 @@
 ones={0:'Zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
 twenties={0:'',10:'ten',20:'twenty',30:'thirty',40:'forty',50:'fifty',60:'sixty',70:'seventy',80:'eighty',90:'ninety'}
 teens={11:'eleven',12:'twelve',13:'thirteen',18:'eighteen'}
-t= input("Enter number:")
+t= input("Enter a 4 digit number:")
 l=[int(x) for x in t]
 
 while l[0]==0:
